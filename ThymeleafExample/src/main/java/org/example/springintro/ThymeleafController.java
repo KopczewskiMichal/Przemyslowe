acca9ba26e8e4fa54254e4f4e901e13a7db318e0
mkopczewski <kopczewski.michal101@gmail.com>
2024-11-20 04:48:50 +0100

    Nagłowek i stopka z Thymeleaf fragments

--- ThymeleafExample/src/main/java/org/example/springintro/ThymeleafController.java
+++ ThymeleafExample/src/main/java/org/example/springintro/ThymeleafController.java
@@ -21,13 +21,6 @@ public class ThymeleafController {
         this.employeeService = employeeService;
     }
 
-
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(Model model) {
-        model.addAttribute("message", "Witaj na stronie Thymeleaf!");
-        return "index";
-    }
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String mainPage(Model model) {
         model.addAttribute("totalEmployees", this.employeeManagementService.getEmployeesCount() + 3); // 3 pracowników kluczowych
@@ -44,6 +37,8 @@ public class ThymeleafController {
     public String getEmployeeById(Model model, @PathVariable int id) {
         Person employee = employeeManagementService.getEmployeeById(id);
         model.addAttribute("employee", employee);
+        model.addAttribute("totalEmployees", this.employeeManagementService.getEmployeesCount() + 3); // 3 pracowników kluczowych
+
         return "employeeDetails";
     }
 
